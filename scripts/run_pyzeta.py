7f040ba5ae6fc7592c25b1fbe786a820f61f1ce9
christofs <c.schoech@gmail.com>
2017-08-18 21:57:51 +0200

    experimental attempt at comparing results from two measures visually

--- scripts/run_pyzeta.py
+++ scripts/run_pyzeta.py
@@ -88,7 +88,7 @@ The calculation can work in several ways: by division, subtraction as well as wi
 
 contrast = ["subgenre", "detective", "historical"]# category, group1, group2; or: "random", "one", "two"
 logaddition= 0.5 # has effect on log calculation.
-calculate.main(datafolder, metadatafile, contrast, logaddition, resultsfolder, segmentlength, featuretype)
+#calculate.main(datafolder, metadatafile, contrast, logaddition, resultsfolder, segmentlength, featuretype)
 
 
 
@@ -103,7 +103,7 @@ This module provides several plotting functionalities.
 # This is for a horizontal barchart for plotting Zeta and similar scores per feature.
 numfeatures = 20
 measure = "origzeta" # origzeta|logzeta|ratiorelfreqs|etc.
-visualize.zetabarchart(segmentlength, featuretype, contrast, measure, numfeatures, resultsfolder, plotfolder)
+#visualize.zetabarchart(segmentlength, featuretype, contrast, measure, numfeatures, resultsfolder, plotfolder)
 
 # This is for a scatterplot showing the relation between indicators and scores.
 numfeatures = 500
@@ -113,6 +113,14 @@ cutoff = 0.3
 
 
 
+# =================================
+# Experimental
+# =================================
+
+import complot
+comparison = ["origzeta", "log2zeta"]
+numfeatures = 20
+complot.comparisonplot(resultsfolder, plotfolder, comparison, numfeatures, segmentlength, featuretype, contrast)
 
 
 
